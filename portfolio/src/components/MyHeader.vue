074e87a85ba4309616d9eef263b20db1c5352b66
HajongChan2 <gktjswn92@naver.com>
2023-12-16 18:44:18 +0900

    app.vue, MyHaeder.vue update

--- portfolio/src/components/MyHeader.vue
+++ portfolio/src/components/MyHeader.vue
@@ -1,28 +1,33 @@
 <template>
-  <nav class="col-10 navbar navbar-expand-lg bg-body-tertiary" id="navbar">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#" @click.prevent="handleClick()">chan's portfolio</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="navber-nav">
+  <div class="headerContainer">
+    <nav class="col-10 navbar navbar-expand-lg bg-body-tertiary" id="navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#" @click.prevent="handleClick()">chan's portfolio</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0" id="navber-nav">
 
-          <li class="nav-item">
-            <a class="nav-link" href="#" @click.prevent="handleClick(0)" :class="{ active: headerIndex == 1 }">Profile</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" @click.prevent="handleClick(1)" :class="{ active: headerIndex == 2 }">Skills</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" @click.prevent="handleClick(2)" :class="{ active: headerIndex == 3 }">Project</a>
-          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" @click.prevent="handleClick(0)"
+                :class="{ active: headerIndex == 1 }">Profile</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" @click.prevent="handleClick(1)"
+                :class="{ active: headerIndex == 2 }">Skills</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#" @click.prevent="handleClick(2)"
+                :class="{ active: headerIndex == 3 }">Project</a>
+            </li>
 
-        </ul>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
   
 <script>
@@ -39,19 +44,22 @@ export default {
 </script>
   
 <style>
-#navbar{
-  background-color: transparent !important;
+.headerContainer{
   max-width: 1920px;
 }
-#navber-nav{
+#navbar {
+  background-color: transparent !important;
+  
+}
+
+#navber-nav {
   margin-right: 0 !important;
 }
 
 @media (max-width: 820px) {
-  #navber-nav{
-    background-color:#fff;
+  #navber-nav {
+    background-color: #fff;
     opacity: 0.8;
   }
-}
-</style>
+}</style>
   
